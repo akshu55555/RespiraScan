f231170cf096c366a0a3ba9c40138bcd3f000f25
Kanak027 <dagade.kanak@gmail.com>
2025-03-22 21:27:42 +0530

    Added Home Page and SignUp Pages without the styling

---
+++ Home_Page/script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling for the About Us section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 20,
+                behavior: "smooth"
+            });
+        }
+    });
+});
